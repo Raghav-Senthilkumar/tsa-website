44b8b8e639f369c804c5586ddb280d2853f13960
Raghav Senthilkumar <s18raghav@gmail.com>
2026-04-28 16:41:48 -0700

    removing dynamic viewport on mobile

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.tsx";
-import { initViewportHeightVar } from "./lib/vh";
-
-initViewportHeightVar();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
